8c474247592ce9e626475dbd40a902a3bda0167c
Adam Budziak <adam.budziak@10clouds.com>
2019-08-30 15:34:47 +0200

    Rewritten all contracts.

--- bc/migrations/2_make_posts.js
+++ bc/migrations/2_make_posts.js
@@ -1,21 +1,21 @@
 const Posts = artifacts.require("Posts");
 const Comments = artifacts.require("Comments");
-const Upvotes = artifacts.require("Upvotes");
-const Downvotes = artifacts.require("Downvotes");
+const PostVotes = artifacts.require("PostVotes");
+const CommentVotes = artifacts.require("CommentVotes");
 const fs = require('fs');
 
 const globalConfig = require('./config/global');
 
 module.exports = async (deployer) => {
     await deployer.deploy(Posts);
-    await deployer.deploy(Comments);
-    await deployer.deploy(Upvotes);
-    await deployer.deploy(Downvotes);
+    await deployer.deploy(Comments, Posts.address);
+    await deployer.deploy(PostVotes, Posts.address);
+    await deployer.deploy(CommentVotes, Posts.address, Comments.address);
 
     fs.writeFileSync(globalConfig.deploymentFile, JSON.stringify({
         Posts: Posts.address,
         Comments: Comments.address,
-        Upvotes: Upvotes.address,
-        Downvotes: Downvotes.address,
+        PostVotes: PostVotes.address,
+        CommentVotes: CommentVotes.address,
     }));
 };
--- bc/test/testPosts.js
+++ bc/test/testPosts.js
@@ -4,17 +4,21 @@ const Posts = artifacts.require('Posts');
 contract('Posts', async accounts => {
     it('Should add a post correctly', async () => {
         let posts = await Posts.deployed();
-        let postsCountBefore = await posts.getPostsCount.call();
+        let postsCountBefore = await posts.getPostCount.call();
         assert.equal(postsCountBefore, 0);
 
-        const date = '2019-07-11T10:05:11.367Z';
+        const fee = await posts.addPostFee.call();
+
+        // the contract  doesn't care whether the hash was computer properly
         const postHash = web3.utils.keccak256('Test content');
-        await posts.addPost(date, postHash, {value: web3.utils.toWei('0.005', 'ether')});
-        let postsCountAfter = await posts.getPostsCount.call();
+        await posts.addPost(postHash, {value: fee});
+        let postsCountAfter = await posts.getPostCount.call();
         assert.equal(postsCountAfter, 1);
 
         let newPost = await posts.posts.call(0);
-        assert.equal(newPost[0], date);
-        assert.equal(newPost[1], postHash);
-    })
+        assert.equal(newPost, postHash);
+
+        let postAuthor = await posts.postToAuthor.call(postHash);
+        assert.equal(postAuthor, accounts[0]);
+    });
 });
