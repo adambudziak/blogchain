6cdbf76a20804fdef3a9f19d8dc1233c09d9786a
Adam Budziak <adam.budziak@10clouds.com>
2019-09-04 14:13:47 +0200

    Fixed broken tavern filters tests

--- backend/tests/functions/utils.py
+++ backend/tests/functions/utils.py
@@ -11,8 +11,15 @@ def save_count(response):
 def with_response_objects(fn):
     def inner(response, *args, **kwargs):
         response_json = response.json()
-        assert response_json['count'] > 0, 'There are no objects in the response.'
-        return fn(response_json['results'], *args, **kwargs)
+        try:
+            count = response_json['count']
+            results = response_json['results']
+        except TypeError:
+            if type(response_json) == list:
+                return fn(response_json, *args, **kwargs)
+        else:
+            assert response_json['count'] > 0, 'There are no objects in the response.'
+            return fn(response_json['results'], *args, **kwargs)
     return inner
 
 
@@ -30,13 +37,6 @@ def assert_all_verified(objects):
 def assert_all_by(objects, author):
     assert all(obj['author'] == author for obj in objects), "The author doesn't match for some of the objects."
 
-
-def list_assert_all_verified(response):
-    assert all(obj['verified'] for obj in response.json()), 'Not all objects are verified.'
-
-
-def list_assert_all_not_verified(response):
-    assert all(not obj['verified'] for obj in response.json()), 'Some objects are verified'
 
 def assert_empty_list(response):
     assert not len(response.json()), "The response list is not empty."
