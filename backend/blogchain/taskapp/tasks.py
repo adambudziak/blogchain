4a2c7a39734293b1282f30d76bc425e166c9803a
Adam Budziak <adambudziak@protonmail.com>
2019-10-08 11:50:37 +0200

    add votes verification

--- backend/blogchain/taskapp/tasks.py
+++ backend/blogchain/taskapp/tasks.py
@@ -3,10 +3,15 @@ from blogchain.taskapp.celery import app
 from blogchain.posts.models import (
     Post,
     Comment,
+    PostVote,
+    CommentVote,
 )
+
 from blogchain.posts.bc.contracts import (
     PostsContract,
     CommentsContract,
+    PostVotesContract,
+    CommentVotesContract,
 )
 
 import logging
@@ -30,4 +35,24 @@ def verify_comments(self):
     verified = comments_contract.verify_comments(comments_to_verify)
 
     logging.info('Verified %d comments out of %d.' % (verified, comments_to_verify.count()))
+    return verified
+
+
+@app.task(name="blogchain.taskapp.tasks.verify_post_votes", bind=True)
+def verify_comments(self):
+    post_votes_to_verify = PostVote.objects.filter(verified=False)
+    post_votes_contract = PostVotesContract.default()
+    verified = post_votes_contract.verify_votes(post_votes_to_verify)
+
+    logging.info('Verified %d post votes out of %d.' % (verified, post_votes_to_verify.count()))
+    return verified
+
+
+@app.task(name="blogchain.taskapp.tasks.verify_comment_votes", bind=True)
+def verify_comments(self):
+    comment_votes_to_verify = CommentVote.objects.filter(verified=False)
+    comment_votes_contract = CommentVotesContract.default()
+    verified = comment_votes_contract.verify_votes(comment_votes_to_verify)
+
+    logging.info('Verified %d comment votes out of %d.' % (verified, comment_votes_to_verify.count()))
     return verified
